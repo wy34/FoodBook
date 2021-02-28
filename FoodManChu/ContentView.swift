081f2e79b1ad14bbd99adf0f3f16026c4d370184
William Yeung <williamyeung@Williams-Mac-mini.local>
2021-02-27 21:44:49 -0500

    First commit

---
+++ FoodManChu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodManChu
+//
+//  Created by William Yeung on 2/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodManChu/FoodManChuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodManChuApp.swift
+//  FoodManChu
+//
+//  Created by William Yeung on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodManChuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
